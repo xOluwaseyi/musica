760d7072af598ba2762b503117beb6badff19a75
Fagbemi Oluwaseyi <seyifagbemi211@gmail.com>
2022-11-04 17:35:45 +0100

    fix: trying to fix nextjs bug

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
-
-// next.config.js
 module.exports = {
   images: {
     domains: ["musica-api.up.railway.app"],
@@ -22,4 +15,6 @@ module.exports = {
 
     return config;
   },
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true
 };
